4c61467d93d8e8978815106e3422de50c7543b51
Malak CHEFRID <malakchefrid@gmail.com>
2023-08-31 16:47:08 -0300

    using os module fix #7

---
+++ directory.py
@@ -0,0 +1,6 @@
+# python has a built-in module that interacts with the operating system it is os
+import os 
+
+print(os.getcwd()) # path of the current working directory
+print(os.listdir()) # list of elements in the current working directory
+
