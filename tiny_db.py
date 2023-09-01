82e67d473226f05edf5b6674d651c2529f3a70b4
Malak CHEFRID <malakchefrid@gmail.com>
2023-09-01 10:57:18 -0300

    advanced fix #8

--- tiny_db.py
+++ tiny_db.py
@@ -23,7 +23,49 @@ db.update({'size': 15}, Fruit.type=='peach')
 #db.remove(Fruit.type=='ananas')
 db.search(Fruit.type=='ananas')
 
-
-
 ##### if you need to delete everything
 ##### db.truncate()
+
+## advanced
+
+db.insert({'country': 'Jamaica', 'country-code': '1-876'})
+user = Query()
+## we can't use the format db.search(user.country-code == '1-876), it is invalid because of the hyphen in country-code
+## we use the following sntax instead
+db.search(user['country-code'] =='1-876')
+
+#using where
+from tinydb import where
+print(db.search(where('type')=='apple'))
+
+# add nested fields
+db.insert({
+  "log": {
+    "1": {
+      "date": "2021-12-11",
+      "split": "legday",
+      "exercises": [
+        {
+          "name": "squat",
+          "sets": [
+            { "set no.": 1, "reps": 8, "weight": "70 kg" },
+            { "set no.": 2, "reps": 8, "weight": "80 kg" },
+            { "set no.": 3, "reps": 5, "weight": "90 kg" },
+            { "set no.": 4, "reps": 5, "weight": "90 kg" }
+          ]
+        },
+        {
+          "name": "deadlift",
+          "sets": [
+            { "set no.": 1, "reps": 8, "weight": "70 kg" },
+            { "set no.": 2, "reps": 8, "weight": "80 kg" },
+            { "set no.": 3, "reps": 5, "weight": "90 kg" },
+            { "set no.": 4, "reps": 5, "weight": "90 kg" }
+          ]
+        }
+      ]
+    }
+  }
+})
+
+print(len(db))
